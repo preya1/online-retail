ab7db10313ca493ff61729f0704fe2eb8bc6255c
Albu Gabriel <albugabriel30@gmail.com>
2019-08-29 19:38:25 +0300

    Sucessfully until now !

--- onlineretail/src/main/java/com/preya/onlineretail/controller/PaginaController.java
+++ onlineretail/src/main/java/com/preya/onlineretail/controller/PaginaController.java
@@ -2,6 +2,7 @@ package com.preya.onlineretail.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,7 +16,13 @@ public class PaginaController {
 		mv.addObject("Salutare","Bine Ai Venit Pe Pagina - Spring MVC");
 		return mv;
 		
-		
+	}
+	
+	@RequestMapping(value = "/test")
+	public ModelAndView test(@RequestParam("Salutare")String Salutare) {
+		ModelAndView mv = new ModelAndView("pagina");
+		mv.addObject("Salutare","Bine Ai Venit Pe Pagina - Spring MVC");
+		return mv;
 	}
 	
 }
